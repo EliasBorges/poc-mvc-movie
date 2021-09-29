da0de0756c60c9b4ac64f15d1cd218aec22efd1c
Elias Borges <eliasborges@unipam.edu.br>
2021-09-28 22:59:43 -0300

    Projeto MvcMovie (DSIA 2/2021)

--- Migrations/20210929015608_rating.Designer.cs
+++ Migrations/20210929015608_rating.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20210929015608_rating")]
+    partial class rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
